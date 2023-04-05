3b0024a65ecc9b7a09ad9e55a5e7c2d06f8be6f1
soollyy <solianatesema1@gmail.com>
2023-04-05 16:02:09 +0100

    fully functional jokes service project

--- index.js
+++ index.js
@@ -7,14 +7,46 @@ app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const { tags, content } = req.query;
+    let jokes = await Joke.findAll({
+      where: {},
+    });
+    
+    if (tags) {
+      const tagArray = tags.split(',');
+      jokes = jokes.filter(joke => {
+        return tagArray.every(tag => joke.tags.includes(tag));
+      });
+    }
+
+    if (content) {
+      const contentArray = content.split(' ')
+      if (Array.isArray(jokes)) {
+        jokes = jokes.filter(joke => { 
+          return contentArray.some(content => joke.joke.includes(content))
+        });
+      } else {
+        jokes = [];
+      }
+    }
+    //because in Joke.js the table defines joke and tags, line 26 should be joke.joke.includes instead of joke.content.includes because there is no content table only joke and tags.
+    
     res.send(jokes);
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
-});
+})
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
+
+
+//TODO:
+//A fully complete GET /jokes route should:
+
+// Return a list of all jokes when no query string parameters are provided
+
+// Return a list of jokes, filtered by tag for GET /jokes?tag=YOUR_QUERY_HERE
+
+// Return a list of jokes, filtered by content GET /jokes?content=YOUR_QUERY_HERE
