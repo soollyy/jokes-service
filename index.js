560385aa564cc1de4dc94713eda4f26a7b46cce9
soollyy <solianatesema1@gmail.com>
2023-04-06 11:20:57 +0100

    extention: POST request test and implementation with comments

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Joke } = require('./db');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.get('/jokes', async (req, res, next) => {
+app.get('/jokes/', async (req, res, next) => {
   try {
     const { tags, content } = req.query;
     let jokes = await Joke.findAll({
@@ -20,16 +20,17 @@ app.get('/jokes', async (req, res, next) => {
     }
 
     if (content) {
-      const contentArray = content.split(' ')
+      const contentArray = content.split(',')
       if (Array.isArray(jokes)) {
         jokes = jokes.filter(joke => { 
-          return contentArray.some(content => joke.joke.includes(content))
+          return contentArray.every(content => joke.joke.includes(content))
         });
       } else {
         jokes = [];
       }
     }
     //because in Joke.js the table defines joke and tags, line 26 should be joke.joke.includes instead of joke.content.includes because there is no content table only joke and tags.
+    //line 26- can be return contentArray.some so its not strict compared to tagArray where it is strict. both .every and .some work the same
     
     res.send(jokes);
   } catch (error) {
@@ -37,6 +38,21 @@ app.get('/jokes', async (req, res, next) => {
     next(error);
   }
 })
+
+app.post('/jokes/', async (req, res, next) => {
+  try {
+    const { setup, punchline } = req.body; // extratcs the setup and punchline fields from the request body
+    const newJoke = await Joke.create({ setup, punchline }); // creates a new joke and add it to the database using the create method of the Joke model
+
+    res.status(200).json({ message: 'Joke added successfully' }); // sends a response indicating that the joke was added successfully
+  } catch (error) { // If an error occurs while adding the joke
+      res.status(500).json({ message: 'Failed to add joke' }); // sends a response with an error message
+  }
+});
+
+app.use(express.json()); // uses middleware to parse the request body
+
+
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
@@ -50,3 +66,25 @@ module.exports = app;
 // Return a list of jokes, filtered by tag for GET /jokes?tag=YOUR_QUERY_HERE
 
 // Return a list of jokes, filtered by content GET /jokes?content=YOUR_QUERY_HERE
+
+
+//EXTENTION:
+
+// Implement one or all of the following routes. Write your process, step by step on each line, dont forget to comment them out ( // )
+
+// POST /jokes: Adds a joke to the database. Should accept both columns in the req.body
+
+// DELETE /jokes/:id: Removes a joke from the database, by ID.
+
+// PUT /jokes/:id: Edits a joke by ID.  Should accept both/either columns in the req.body
+
+//For the DELETE and PUT routes, be sure to send back an error (by calling next) if no joke exists that matches the ID (i.e. if it was previously deleted, or if it was never added.
+
+
+
+// FURTHER EXTENSION:
+
+//As an added challenge, try your hand at writing tests for the above routes! Use the preexisting tests as a guide
+
+
+
--- index.test.js
+++ index.test.js
@@ -7,27 +7,37 @@ const seedData = require('./db/seedData');
 describe('GET /jokes', () => {
     beforeAll(async () => {
         await sequelize.sync({ force: true }); // recreate db
-        await seed();
+        await seed()
     });
 
     it('should return a list of all jokes', async () => {
-        const response = await request(app).get('/jokes');
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveLength(seedData.length);
-        expect(response.body[0]).toEqual(expect.objectContaining(seedData[0]));
+        const response = await request(app).get('/jokes')
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveLength(seedData.length)
+        expect(response.body[0]).toEqual(expect.objectContaining(seedData[0]))
     });
 
     it('should return a list of jokes, filtered by tag', async () => {
-        const response = await request(app).get('/jokes?tags=anatomy');
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveLength(3);
-        expect(response.body[0]).toEqual(expect.objectContaining(seedData[3]));
+        const response = await request(app).get('/jokes?tags=anatomy')
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveLength(3)
+        expect(response.body[0]).toEqual(expect.objectContaining(seedData[3]))
     });
 
     it('should return a list of jokes, filtered by content', async () => {
-        const response = await request(app).get('/jokes?content=flamingo');
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveLength(1);
-        expect(response.body[0]).toEqual(expect.objectContaining(seedData[2]));
+        const response = await request(app).get('/jokes?content=flamingo')
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveLength(1)
+        expect(response.body[0]).toEqual(expect.objectContaining(seedData[2]))
     });
 });
+
+describe('POST /jokes', () => {
+    it('should add a joke to the database', async () => {
+        const joke = { setup: 'example joke?', punchline: 'example answer.'}
+        const response = await request(app)
+        .post('/jokes') //calls on the request object that specifies the HTTP method and endpoint for the request
+        .send(joke);//calls on the request object that sends the joke object in the body of the HTTP request. 
+
+    })
+})
