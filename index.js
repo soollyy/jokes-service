b559a6ae0b454b0e649e8320011df12981616f51
DamienCook <dacook@brewdigital.uk>
2023-04-11 11:46:28 +0100

    added delete request

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Joke } = require('./db');
+const { Joke } = require("./db");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/jokes/', async (req, res, next) => {
+app.get("/jokes/", async (req, res, next) => {
   try {
     const { tags, content } = req.query;
     let jokes = await Joke.findAll({
       where: {},
     });
-    
+
     if (tags) {
-      const tagArray = tags.split(',');
-      jokes = jokes.filter(joke => {
-        return tagArray.every(tag => joke.tags.includes(tag));
+      const tagArray = tags.split(",");
+      jokes = jokes.filter((joke) => {
+        return tagArray.every((tag) => joke.tags.includes(tag));
       });
     }
 
     if (content) {
-      const contentArray = content.split(',')
+      const contentArray = content.split(",");
       if (Array.isArray(jokes)) {
-        jokes = jokes.filter(joke => { 
-          return contentArray.every(content => joke.joke.includes(content))
+        jokes = jokes.filter((joke) => {
+          return contentArray.every((content) => joke.joke.includes(content));
         });
       } else {
         jokes = [];
@@ -31,32 +31,38 @@ app.get('/jokes/', async (req, res, next) => {
     }
     //because in Joke.js the table defines joke and tags, line 26 should be joke.joke.includes instead of joke.content.includes because there is no content table only joke and tags.
     //line 26- can be return contentArray.some so its not strict compared to tagArray where it is strict. both .every and .some work the same
-    
+
     res.send(jokes);
   } catch (error) {
     console.error(error);
     next(error);
   }
-})
+});
 
-app.post('/jokes/', async (req, res, next) => {
+app.post("/jokes/", async (req, res, next) => {
   try {
     const { setup, punchline } = req.body; // extratcs the setup and punchline fields from the request body
     const newJoke = await Joke.create({ setup, punchline }); // creates a new joke and add it to the database using the create method of the Joke model
 
-    res.status(200).json({ message: 'Joke added successfully' }); // sends a response indicating that the joke was added successfully
-  } catch (error) { // If an error occurs while adding the joke
-      res.status(500).json({ message: 'Failed to add joke' }); // sends a response with an error message
+    res.status(200).json({ message: "Joke added successfully" }); // sends a response indicating that the joke was added successfully
+  } catch (error) {
+    // If an error occurs while adding the joke
+    res.status(500).json({ message: "Failed to add joke" }); // sends a response with an error message
   }
 });
 
 app.use(express.json()); // uses middleware to parse the request body
 
-
+app.delete("/jokes/:id", async (req, res) => {
+  const { id } = req.params;
+  await Joke.destroy({
+    where: { id },
+  });
+  res.status(204).end();
+});
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
-
 
 //TODO:
 //A fully complete GET /jokes route should:
@@ -66,7 +72,6 @@ module.exports = app;
 // Return a list of jokes, filtered by tag for GET /jokes?tag=YOUR_QUERY_HERE (done)
 
 // Return a list of jokes, filtered by content GET /jokes?content=YOUR_QUERY_HERE (done)
-
 
 //EXTENTION:
 
@@ -80,11 +85,6 @@ module.exports = app;
 
 //For the DELETE and PUT routes, be sure to send back an error (by calling next) if no joke exists that matches the ID (i.e. if it was previously deleted, or if it was never added.
 
-
-
 // FURTHER EXTENSION:
 
 //As an added challenge, try your hand at writing tests for the above routes! Use the preexisting tests as a guide
-
-
-
--- index.test.js
+++ index.test.js
@@ -1,43 +1,55 @@
-const app = require('./index');
-const { sequelize, Joke } = require('./db');
-const request = require('supertest');
-const seed = require('./db/seedFn');
-const seedData = require('./db/seedData');
+const app = require("./index");
+const { sequelize, Joke } = require("./db");
+const request = require("supertest");
+const seed = require("./db/seedFn");
+const seedData = require("./db/seedData");
 
-describe('GET /jokes', () => {
-    beforeAll(async () => {
-        await sequelize.sync({ force: true }); // recreate db
-        await seed()
-    });
+describe("GET /jokes", () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true }); // recreate db
+    await seed();
+  });
 
-    it('should return a list of all jokes', async () => {
-        const response = await request(app).get('/jokes')
-        expect(response.status).toBe(200)
-        expect(response.body).toHaveLength(seedData.length)
-        expect(response.body[0]).toEqual(expect.objectContaining(seedData[0]))
-    });
+  it("should return a list of all jokes", async () => {
+    const response = await request(app).get("/jokes");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(seedData.length);
+    expect(response.body[0]).toEqual(expect.objectContaining(seedData[0]));
+  });
 
-    it('should return a list of jokes, filtered by tag', async () => {
-        const response = await request(app).get('/jokes?tags=anatomy')
-        expect(response.status).toBe(200)
-        expect(response.body).toHaveLength(3)
-        expect(response.body[0]).toEqual(expect.objectContaining(seedData[3]))
-    });
+  it("should return a list of jokes, filtered by tag", async () => {
+    const response = await request(app).get("/jokes?tags=anatomy");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(3);
+    expect(response.body[0]).toEqual(expect.objectContaining(seedData[3]));
+  });
 
-    it('should return a list of jokes, filtered by content', async () => {
-        const response = await request(app).get('/jokes?content=flamingo')
-        expect(response.status).toBe(200)
-        expect(response.body).toHaveLength(1)
-        expect(response.body[0]).toEqual(expect.objectContaining(seedData[2]))
-    });
+  it("should return a list of jokes, filtered by content", async () => {
+    const response = await request(app).get("/jokes?content=flamingo");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(1);
+    expect(response.body[0]).toEqual(expect.objectContaining(seedData[2]));
+  });
 });
 
-describe('POST /jokes', () => {
-    it('should add a joke to the database', async () => {
-        const joke = { setup: 'example joke?', punchline: 'example answer.'}
-        const response = await request(app)
-        .post('/jokes') //calls on the request object that specifies the HTTP method and endpoint for the request
-        .send(joke);//calls on the request object that sends the joke object in the body of the HTTP request. 
+describe("POST /jokes", () => {
+  it("should add a joke to the database", async () => {
+    const joke = { setup: "example joke?", punchline: "example answer." };
+    const response = await request(app)
+      .post("/jokes") //calls on the request object that specifies the HTTP method and endpoint for the request
+      .send(joke); //calls on the request object that sends the joke object in the body of the HTTP request.
+  });
+});
 
-    })
-})
+describe("DELETE /jokes/:id", () => {
+  it("should delete a joke from the database", async () => {
+    const joke = await Joke.create({
+      setup: "example joke?",
+      punchline: "example answer.",
+    }); // Create a joke in the database to delete
+    const response = await request(app).delete(`/jokes/${joke.id}`); // calls on the request object that specifies the HTTP method and endpoint for the request, with the ID of the joke to delete
+    expect(response.status).toBe(204); // checks that the response status is 204 No Content, indicating successful deletion
+    const deletedJoke = await Joke.findByPk(joke.id); // Try to find the deleted joke in the database
+    expect(deletedJoke).toBeNull(); // Checks that the deleted joke cannot be found, indicating successful deletion
+  });
+});
